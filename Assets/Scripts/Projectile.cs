f805af4be18dce0f561c3a5b5e79cc5b85a14fe5
finalmegaman NA <finalmegaman@gmail.com>
2019-07-18 20:40:02 -0700

    136 Make Projectile Move

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	[SerializeField] float speed = 1f;
+
+	void Update () {
+		transform.Translate(Vector2.right * Time.deltaTime * speed);
+	}
+}
