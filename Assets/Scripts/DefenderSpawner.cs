2f9ac624a2f2f5d421e2b035f9c30bbfc36315fe
finalmegaman NA <finalmegaman@gmail.com>
2019-07-25 21:07:09 -0700

    141 Spawn At Mouse Position.

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour {
+
+	[SerializeField] GameObject defender;
+
+	private void OnMouseDown() {
+		SpawnDefender(GetSquareClicked());
+	}
+
+	private Vector2 GetSquareClicked() {
+		Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+		Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+
+		return worldPos;
+	}
+
+	private void SpawnDefender(Vector2 worldPos){
+		GameObject newDefender = Instantiate(defender, worldPos, Quaternion.identity) as GameObject;
+	}
+}
