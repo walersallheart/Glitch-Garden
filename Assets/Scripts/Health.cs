41af03f3b0b6de4202ccf3de8caa8b79dc72f23b
finalmegaman NA <finalmegaman@gmail.com>
2019-07-25 20:46:35 -0700

    139 Tidy Up & Death FX

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	[SerializeField] float health = 100f;
+	[SerializeField] GameObject deathVFX;
+
+	public void DealDamage(float damage){
+		health -= damage;
+
+		if (health <= 0) {
+			TriggerDeathVFX();
+			Destroy(gameObject);
+		}
+	}
+
+    private void TriggerDeathVFX()
+    {
+        if (!deathVFX) { return; }
+
+		GameObject deathVFXObject = Instantiate(deathVFX, transform.position, Quaternion.identity);
+		Destroy(deathVFXObject, 1f);
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
 
 	[SerializeField] float speed = 1f;
+	[SerializeField] float damage = 50f;
 
 	void Update () {
 		transform.Translate(Vector2.right * Time.deltaTime * speed,Space.World);
 		transform.Rotate(Vector3.forward, 360f * Time.deltaTime);
+	}
+
+	private void OnTriggerEnter2D(Collider2D otherCollider) {
+		Debug.Log("I hit:" + otherCollider.name);
+
+		var health = otherCollider.GetComponent<Health>();
+		var attacker = otherCollider.GetComponent<Attacker>();
+
+		if (attacker && health) {
+			health.DealDamage(damage);
+			Destroy(gameObject);
+		}
 	}
 }
