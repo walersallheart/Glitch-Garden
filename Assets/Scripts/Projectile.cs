6d278295a79bc609a579f9c6e41456686b3c55cd
finalmegaman NA <finalmegaman@gmail.com>
2019-07-25 19:54:30 -0700

    136 Make Projectile Move

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,6 +7,7 @@ public class Projectile : MonoBehaviour {
 	[SerializeField] float speed = 1f;
 
 	void Update () {
-		transform.Translate(Vector2.right * Time.deltaTime * speed);
+		transform.Translate(Vector2.right * Time.deltaTime * speed,Space.World);
+		transform.Rotate(Vector3.forward, 360f * Time.deltaTime);
 	}
 }
