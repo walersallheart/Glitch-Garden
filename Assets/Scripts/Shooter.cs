238e8712085a4d784266a11b626cca2d55c726e3
finalmegaman NA <finalmegaman@gmail.com>
2019-08-20 19:21:07 -0700

    152 Attack In My Lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,6 +5,42 @@ using UnityEngine;
 public class Shooter : MonoBehaviour {
 
 	[SerializeField] GameObject projectile, gun;
+
+	AttackerSpawner myLaneSpawner;
+
+	private void Start() {
+		SetLaneSpawner();
+	}
+
+	private void Update() {
+		if (IsAttackerInLane()){
+			Debug.Log("Shoot pew pew");
+		} else {
+			Debug.Log("Sit and Wait");
+		}
+	}
+
+	private void SetLaneSpawner(){
+		AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+		foreach (AttackerSpawner spawner in spawners)
+		{
+			bool IsCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+
+			if (IsCloseEnough) {
+				myLaneSpawner = spawner;
+			}
+		}
+	}
+
+	private bool IsAttackerInLane(){
+		if (myLaneSpawner.transform.childCount <= 0) {
+			return false;
+		}
+
+		return true;
+	}
+
 	public void Fire(){
 		Instantiate(projectile,gun.transform.position, gun.transform.rotation);
 	}
