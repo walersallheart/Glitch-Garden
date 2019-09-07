6517de5eaf03ef90fbeb99d7f88fb1bffd6acf4f
finalmegaman NA <finalmegaman@gmail.com>
2019-09-07 15:31:41 -0700

    157 Make Our Attackers Attack

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -6,11 +6,18 @@ public class Attacker : MonoBehaviour {
 
 	float currentSpeed = 1f;
 
+	GameObject currentTarget;
+
 	public void SetMovementSpeed(float speed){
 		currentSpeed = speed;
 	}
 
 	void Update () {
 		transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+	}
+
+	public void Attack(GameObject target){
+		GetComponent<Animator>().SetBool("isAttacking",true);
+		currentTarget = target;
 	}
 }
