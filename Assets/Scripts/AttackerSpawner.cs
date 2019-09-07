c88026a7d6037923191c53de00504fb06dc00307
finalmegaman NA <finalmegaman@gmail.com>
2019-09-07 16:30:14 -0700

    160 Array For Spawning Attackers

--- Assets/Scripts/AttackerSpawner.cs
+++ Assets/Scripts/AttackerSpawner.cs
@@ -8,7 +8,7 @@ public class AttackerSpawner : MonoBehaviour {
 
 	[SerializeField] float minSpawnDelay = 1f;
 	[SerializeField] float maxSpawnDelay = 5f;
-	[SerializeField] Attacker attackerPrefab;
+	[SerializeField] Attacker[] attackerPrefabArray;
 
 	IEnumerator Start () {
 		while (spawn) {
@@ -19,13 +19,13 @@ public class AttackerSpawner : MonoBehaviour {
 	}
 
 	private void SpawnAttacker(){
-		Attacker newAttacker = Instantiate(attackerPrefab, transform.position, Quaternion.identity) as Attacker;
-
-		newAttacker.transform.parent = transform;
+		var attackerIndex = Random.Range(0,attackerPrefabArray.Length);
+		Spawn(attackerPrefabArray[attackerIndex]);
 	}
 
-	// Update is called once per frame
-	void Update () {
+	private void Spawn(Attacker myAttacker){
+		Attacker newAttacker = Instantiate(myAttacker, transform.position, Quaternion.identity) as Attacker;
 
+		newAttacker.transform.parent = transform;
 	}
 }
