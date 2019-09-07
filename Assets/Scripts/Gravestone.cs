2a154aa0d01d384668248cc8690a6065c9d40869
finalmegaman NA <finalmegaman@gmail.com>
2019-09-07 16:19:09 -0700

    159 Fantastic Mr Fox

---
+++ Assets/Scripts/Gravestone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravestone : MonoBehaviour {
+	private void OnTriggerStay2D(Collider2D otherCollider) {
+		Attacker attacker = otherCollider.GetComponent<Attacker>();
+
+		if (attacker){
+			//TODO do some sort of animation
+		}
+	}
+}
