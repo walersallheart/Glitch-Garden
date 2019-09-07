2e916cf1feea88549af6cfca986ca401fb19ca94
finalmegaman NA <finalmegaman@gmail.com>
2019-09-07 15:47:29 -0700

    158 Attackers Striking Target

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -14,10 +14,29 @@ public class Attacker : MonoBehaviour {
 
 	void Update () {
 		transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+		UpdateAnimationState();
+	}
+
+	private void UpdateAnimationState(){
+		if (!currentTarget){
+			GetComponent<Animator>().SetBool("isAttacking",false);
+		}
 	}
 
 	public void Attack(GameObject target){
 		GetComponent<Animator>().SetBool("isAttacking",true);
 		currentTarget = target;
+	}
+
+	public void StrikeCurrentTarget(float damage){
+		if (!currentTarget){
+			return;
+		}
+
+		Health health = currentTarget.GetComponent<Health>();
+
+		if (health) {
+			health.DealDamage(damage);
+		}
 	}
 }
