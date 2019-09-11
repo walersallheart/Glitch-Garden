b10a6ad60f3f22278e30bad5cef2fb5bcbcd50ac
finalmegaman NA <finalmegaman@gmail.com>
2019-09-10 19:21:15 -0700

    161 How To Be A Loser - 162 Create A Level Timer.

---
+++ Assets/Scripts/DamageCollider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageCollider : MonoBehaviour {
+	private void OnTriggerEnter2D(Collider2D other) {
+		FindObjectOfType<LivesDisplay>().TakeLife();
+	}
+}
--- Assets/Scripts/LevelLoad.cs
+++ Assets/Scripts/LevelLoad.cs
@@ -28,8 +28,7 @@ public class LevelLoad : MonoBehaviour {
 		SceneManager.LoadScene(currentSceneIndex + 1);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public void LoadYouLose(){
+		SceneManager.LoadScene("Lose Screen");
 	}
 }
