5507eeed0574e0b78ca2104785a12c6ffd7cc370
finalmegaman NA <finalmegaman@gmail.com>
2019-09-26 20:21:08 -0700

    167 Start Our Options Screen

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -34,6 +34,10 @@ public class LevelLoader : MonoBehaviour {
 		SceneManager.LoadScene("Start Screen");
 	}
 
+	public void LoadOptionsScreen(){
+		SceneManager.LoadScene("Options Screen");
+	}
+
 	public void LoadNextScene(){
 		SceneManager.LoadScene(currentSceneIndex + 1);
 	}
