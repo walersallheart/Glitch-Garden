b2231ed01114c6b10e077088854c538d044c4e09
finalmegaman NA <finalmegaman@gmail.com>
2019-10-05 18:00:37 -0700

    171 Quick Bug Fix - Win Condition.

--- Assets/Scripts/DamageCollider.cs
+++ Assets/Scripts/DamageCollider.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageCollider : MonoBehaviour {
-	private void OnTriggerEnter2D(Collider2D other) {
+	private void OnTriggerEnter2D(Collider2D otherCollider) {
 		FindObjectOfType<LivesDisplay>().TakeLife();
+
+		Destroy(otherCollider.gameObject);
 	}
 }
